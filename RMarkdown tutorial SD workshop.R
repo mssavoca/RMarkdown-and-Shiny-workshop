7c2a7fcf9c4e63c16df0adc006dac1cd8ab6e25c
Matthew Savoca <msavoca13@gmail.com>
2018-01-31 09:48:43 -0800

    starting the workshop

--- RMarkdown tutorial SD workshop.R
+++ RMarkdown tutorial SD workshop.R
@@ -2,3 +2,24 @@
 # R Markdown and Shiny workshop, day 1
 ######################################
 
+from_cran <- c("shiny", "rmarkdown", 
+               "DT", "devtools", "flexdashboard", "gapminder",
+               "rticles", "shinydashboard", "shinythemes", 
+               "tidyverse", "tufte", "xaringan")
+
+install.packages(from_cran, repos = "http://cran.rstudio.com")
+
+# Load
+
+library(shiny)
+library(rmarkdown)
+library(DT)
+library(devtools)
+library(flexdashboard)
+library(gapminder)
+library(rticles)
+library(shinydashboard)
+library(shinythemes)
+library(tidyverse)
+library(tufte)
+library(xaringan)
